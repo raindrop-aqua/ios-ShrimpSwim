9c3625b571dc5675a8740688f8e3e6861e0e2164
Masahiro Atsumi <raindro.aqua@gmail.com>
2019-03-24 19:52:42 +0900

    前準備完了

--- ios-ShrimpSwim/GameViewController.swift
+++ ios-ShrimpSwim/GameViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
@@ -16,10 +15,10 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
+
                 scene.scaleMode = .aspectFill
+                scene.size = view.frame.size
                 
                 // Present the scene
                 view.presentScene(scene)
