97480d7b91b532df4e18435f6f7ef3789099181b
Masahiro Atsumi <raindro.aqua@gmail.com>
2019-03-25 19:41:23 +0900

    section 4.8 finished

--- ios-ShrimpSwim/GameScene.swift
+++ ios-ShrimpSwim/GameScene.swift
@@ -9,22 +9,30 @@ import Foundation
 import SpriteKit
 
 class GameScene: SKScene {
+
+    struct Constants {
+        // Player画像
+        static let PlayerImages = ["shrimp01", "shrimp02", "shrimp03", "shrimp04"]
+    }
     
+    /// 衝突の判定に使うBitMask
     struct ColliderType {
         static let Player: UInt32 = (1 << 0)
-        static let World: UInt32 = (1 << 1)
-        static let Coral: UInt32 = (1 << 2)
-        static let Score: UInt32 = (1 << 3)
-        static let None: UInt32 = (1 << 4)
+        static let World: UInt32  = (1 << 1)
+        static let Coral: UInt32  = (1 << 2)
+        static let Score: UInt32  = (1 << 3)
+        static let None: UInt32   = (1 << 4)
     }
     
     var baseNode: SKNode!
     var coralNode: SKNode!
 
-    
-    
+    var player: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        
+        // 親ノードを作成
         baseNode = SKNode()
         baseNode.speed = 1.0
         self.addChild(baseNode)
@@ -35,9 +43,17 @@ class GameScene: SKScene {
         self.setupBackground()
         self.setupBackgroundRock()
         self.setupCeilingAndLand()
+        self.setupPlayer()
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            player.physicsBody?.velocity = CGVector.zero
+            player.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 23.0))
+        }
+    
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -139,5 +155,35 @@ class GameScene: SKScene {
             sprite.run(repeatForeverLandAnim)
             baseNode.addChild(sprite)
         }
+    }
+
+    func setupPlayer() {
+        var playerTexture = [SKTexture]()
+        
+        for imageName in Constants.PlayerImages {
+            let texture = SKTexture(imageNamed: imageName)
+            texture.filteringMode = .linear
+            playerTexture.append(texture)
+        }
+        
+        let playerAnimation = SKAction.animate(with: playerTexture, timePerFrame: 0.2)
+        let loopAnimation = SKAction.repeatForever(playerAnimation)
+
+        player = SKSpriteNode(texture: playerTexture[0])
+        player.position = CGPoint(x: self.frame.size.width * 0.35, y: self.frame.size.height * 0.6)
+        player.run(loopAnimation)
+        
+        // 物理シミュレーションを設定
+        player.physicsBody = SKPhysicsBody(texture: playerTexture[0], size: playerTexture[0].size())
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.allowsRotation = false
+        
+        // 自分自身にPlayerカテゴリを設定
+        player.physicsBody?.categoryBitMask = ColliderType.Player
+        // 衝突相手にWorldとCoralカテゴリを設定
+        player.physicsBody?.collisionBitMask = ColliderType.World | ColliderType.Coral
+        player.physicsBody?.contactTestBitMask = ColliderType.World | ColliderType.Coral
+
+        self.addChild(player)
     }
 }
